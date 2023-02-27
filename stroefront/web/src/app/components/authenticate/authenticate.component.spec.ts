11597f880a56650589677b6825f14063df32bd48
Abdulrahman Alyahya <al_yahya179@hotmail.com>
2023-02-28 01:39:28 +0300

    format web by running format:fix

--- stroefront/web/src/app/components/authenticate/authenticate.component.spec.ts
+++ stroefront/web/src/app/components/authenticate/authenticate.component.spec.ts
@@ -1,12 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
 import { HttpClient } from '@angular/common/http';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CreateAccountFormComponent } from '../create-account-form/create-account-form.component';
 import { LoginFormComponent } from '../login-form/login-form.component';
-import { SecureAuthenticationComponent } from '../secure-authentication/secure-authentication.component';
 import { AuthenticateComponent } from './authenticate.component';
 
 describe('AuthenticateComponent', () => {
@@ -17,11 +18,16 @@ describe('AuthenticateComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ FormsModule, ReactiveFormsModule, HttpClientTestingModule, RouterTestingModule, NgxSpinnerModule ],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+        RouterTestingModule,
+        NgxSpinnerModule
+      ],
       // we have to declare ALL the child components recursively used by the parent component in order for the test to pass without NG0304 errors
-      declarations: [ AuthenticateComponent, SecureAuthenticationComponent, CreateAccountFormComponent, LoginFormComponent ]
-    })
-    .compileComponents();
+      declarations: [AuthenticateComponent, LoginFormComponent]
+    }).compileComponents();
   });
 
   beforeEach(() => {
--- stroefront/web/src/environments/environment.development.ts
+++ stroefront/web/src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-    apiBaseUrl: 'http://api-postgresql-dev.eba-x4rqbqmr.us-east-1.elasticbeanstalk.com/api' // define the production environment API URL
-    // apiBaseUrl: 'http://localhost:3001/api'
+  apiBaseUrl:
+    'http://api-postgresql-dev.eba-x4rqbqmr.us-east-1.elasticbeanstalk.com/api' // define the production environment API URL
+  // apiBaseUrl: 'http://localhost:3001/api'
 };
