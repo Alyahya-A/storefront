f3903ae46135a2517fce98db1292bd1d4c2c0a6f
Abdulrahman Alyahya <al_yahya179@hotmail.com>
2023-02-28 20:22:06 +0300

    Set env variables from deploy.sh

--- storefront/api/bin/deploy.sh
+++ storefront/api/bin/deploy.sh
@@ -3,3 +3,10 @@
 eb init api-postgresql --region us-east-1
 eb use api-postgresql-dev
 eb deploy api-postgresql-dev
+eb setenv POSTGRES_HOST=$POSTGRES_HOST
+eb setenv POSTGRES_PORT=$POSTGRES_PORT
+eb setenv POSTGRES_DB=$POSTGRES_DB
+eb setenv POSTGRES_USER=$POSTGRES_USER
+eb setenv JWT_SECRET=$JWT_SECRET
+eb setenv SaltRounds=$SaltRounds
+eb setenv TEST=$TEST
